aeb1057009292605afd37888586be354a12df272
TALLIY <mohammadaltalli@gmail.com>
2024-09-27 01:58:15 +0200

    fix

--- launch.sh
+++ launch.sh
@@ -8,10 +8,11 @@ git clone https://github.com/TALLIY/BA.git "$TARGET_DIR"
 
 cd "$TARGET_DIR" || exit 1 
 
+conda activate bachelor-arbeit
 
 export PYTHONPATH="$(pwd)"
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 export TRAIN_DENSE_NETWORK=0
 export LAYER_SIZE=20
@@ -23,15 +24,15 @@ mkdir -p "$(pwd)/rosenbrock/datasets"
 
 export NUMBER_OF_DATAPOINTS=10000000
 export DATASET_PATH="$(pwd)/rosenbrock/datasets/rosenbrock_training_data.csv"
-python3 $(pwd)/rosenbrock/generate_data/generate_data.py
+python $(pwd)/rosenbrock/generate_data/generate_data.py
 
 export NUMBER_OF_DATAPOINTS=10000
 export DATASET_PATH="$(pwd)/rosenbrock/datasets/rosenbrock_testing_data.csv"
-python3 $(pwd)/rosenbrock/generate_data/generate_data.py
+python $(pwd)/rosenbrock/generate_data/generate_data.py
 
 
-python3 $(pwd)/rosenbrock/train_network.py
+python $(pwd)/rosenbrock/train_network.py
 
 export BATCH_SIZE=1
-python3 $(pwd)/rosenbrock/test_network.py
+python $(pwd)/rosenbrock/test_network.py
 
