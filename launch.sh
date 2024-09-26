2d3834c6a5424ed3d6fe215407bbd75e5ae9c3d0
TALLIY <mohammadaltalli@gmail.com>
2024-09-27 01:24:26 +0200

    script update

--- launch.sh
+++ launch.sh
@@ -10,10 +10,28 @@ cd "$TARGET_DIR" || exit 1
 
 
 export PYTHONPATH="$(pwd)"
+
+pip3 install -r requirements.txt
+
 export TRAIN_DENSE_NETWORK=0
+export LAYER_SIZE=20
+export NUM_EPOCHS=200
+export BATCH_SIZE=1000
+export LEARNING_RATE=0.01
 
-pip install -r requirements.txt
+mkdir -p "$(pwd)/rosenbrock/datasets"
 
-mkdir -p "./rosenbrock/datasets"
-python $(pwd)/rosenbrock/generate_data/generate_data.py
-python $(pwd)/rosenbrock/train_network.py
+export NUMBER_OF_DATAPOINTS=10000000
+export DATASET_PATH="$(pwd)/rosenbrock/datasets/rosenbrock_training_data.csv"
+python3 $(pwd)/rosenbrock/generate_data/generate_data.py
+
+export NUMBER_OF_DATAPOINTS=10000
+export DATASET_PATH="$(pwd)/rosenbrock/datasets/rosenbrock_testing_data.csv"
+python3 $(pwd)/rosenbrock/generate_data/generate_data.py
+
+
+python3 $(pwd)/rosenbrock/train_network.py
+
+export BATCH_SIZE=1
+python3 $(pwd)/rosenbrock/test_network.py
+
